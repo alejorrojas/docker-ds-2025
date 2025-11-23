7270124cb5020c8946efa31d8b7e2d216b94420b
JeroZapata <jerozapata13@gmail.com>
2025-11-23 16:10:28 -0300

    fix(api-gateway): delete compras from nginx

--- api-gateway/entrypoint.sh
+++ api-gateway/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Generar nginx.conf desde el template usando envsubst
 # Solo reemplaza las variables definidas en el template
 echo "🔧 Generando configuración de nginx desde template..."
-envsubst '${STOCK_SERVICE_HOST} ${STOCK_SERVICE_PORT} ${COMPRAS_SERVICE_HOST} ${COMPRAS_SERVICE_PORT} ${LOGISTICA_SERVICE_HOST} ${LOGISTICA_SERVICE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${STOCK_SERVICE_HOST} ${STOCK_SERVICE_PORT} ${LOGISTICA_SERVICE_HOST} ${LOGISTICA_SERVICE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Mostrar configuración generada (solo upstreams para debug)
 echo "📋 Configuración de upstreams:"
